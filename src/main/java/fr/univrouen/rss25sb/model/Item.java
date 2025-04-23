f313880a083ab40454abf39f4f167593e0b33910
kamy1tb <jk_taibi@esi.dz>
2025-04-23 08:43:59 +0200

    initial commit

---
+++ src/main/java/fr/univrouen/rss25sb/model/Item.java
@@ -0,0 +1,27 @@
+package fr.univrouen.rss25sb.model;
+
+import jakarta.xml.bind.annotation.*;
+
+@XmlRootElement(name = "item")
+@XmlAccessorType(XmlAccessType.PROPERTY)
+public class Item {
+    @XmlAttribute
+    private String guid;
+    @XmlElement
+    private String title;
+    @XmlElement
+    private String published;
+    public Item(String guid, String title, String published) {
+        super();
+        this.guid = guid;
+        this.title = title;
+        this.published = published;
+    }
+    public Item() {
+    }
+    @Override
+    public String toString() {
+        return ("Article : " + title + "\n(" + guid
+                + ") Le = " + published );
+    }
+}
