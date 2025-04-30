cdc24c885f144645afdec0437fdd3e299dbc63ba
kamy1tb <jk_taibi@esi.dz>
2025-04-30 12:28:01 +0200

    adding landing page

--- src/main/java/fr/univrouen/rss25sb/Rss25SbApplication.java
+++ src/main/java/fr/univrouen/rss25sb/Rss25SbApplication.java
@@ -3,11 +3,12 @@ package fr.univrouen.rss25sb;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class Rss25SbApplication {
 
     public static void main(String[] args) {
-     //   System.getProperties().put("server.port", 8100);
+        System.getProperties().put("server.port", 8100);
         SpringApplication.run(Rss25SbApplication.class, args);
     }
 
--- src/main/java/fr/univrouen/rss25sb/controllers/IndexController.java
+++ src/main/java/fr/univrouen/rss25sb/controllers/IndexController.java
@@ -1,11 +1,17 @@
 package fr.univrouen.rss25sb.controllers;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
-@RestController
+import org.springframework.ui.Model;
+@Controller
 public class IndexController {
     @GetMapping("/")
-    public String index() {
-        return "Hello rss25SB !";
+    public String home(Model model) {
+        // Ajoute des données au modèle
+        model.addAttribute("projectName", "Projet XML RSS25");
+        model.addAttribute("version", "1.0.0");
+        model.addAttribute("developer", "Kamyl Taibi");
+        model.addAttribute("universityLogo", "https://www.choisirlanormandie.fr/app/uploads/2024/08/logo-universite-de-rouen.png");
+        return "landing"; // Nom du fichier Thymeleaf (index.html)
     }
 }
