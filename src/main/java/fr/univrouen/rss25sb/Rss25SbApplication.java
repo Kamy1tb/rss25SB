b4901af6a85af5bb2b5685d69ac9fc20d8b60456
kamy1tb <jk_taibi@esi.dz>
2025-05-27 11:27:56 +0200

    fix : passer du port 8081 au port 8080

--- src/main/java/fr/univrouen/rss25sb/Rss25SbApplication.java
+++ src/main/java/fr/univrouen/rss25sb/Rss25SbApplication.java
@@ -8,7 +8,7 @@ public class Rss25SbApplication {
 
     public static void main(String[] args) {
         // Set server port
-        System.getProperties().put("server.port", 8081);
+        System.getProperties().put("server.port", 8080);
         SpringApplication.run(Rss25SbApplication.class, args);
     }
 
